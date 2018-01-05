365d7fe2ec56378d226f0c865a445378b7b84635
高橋篤史 <AtsushiTakahashi@takahashiatsushi-no-MacBook-Pro.local>
2018-01-06 08:51:58 +0900

    messages_controller_action
    送信用のアクションをコントローラーに作成
    メンバー表示用の変数をビューに移動(動作確認済)

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,10 +1,21 @@
 class MessagesController < ApplicationController
+	before_action :set_group
 
 	def index
-		@group = Group.find(params[:group_id])
-		@members = @group.users
+		@message = Message.new
+		@messages = @group.messages.includes(:user)
 	end
 
 	def create
+		@messages = @group.messages.new(messages_params)
+	end
+
+	private
+	def messages_params
+		params.require(:messages).permit(:body, :image).merge(user_id: current_user.id)
+	end
+
+	def set_group
+		@group = Group.find(params[:group_id])
 	end
 end
