345e7e1ce1d5b05673477354a9c1384203444ee5
高橋篤史 <AtsushiTakahashi@takahashiatsushi-no-MacBook-Pro.local>
2018-01-06 11:30:32 +0900

    indent
    タブ文字を気づいた分修正

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,21 +1,21 @@
 class MessagesController < ApplicationController
-	before_action :set_group
+  before_action :set_group
 
-	def index
-		@message = Message.new
-		@messages = @group.messages.includes(:user)
-	end
+  def index
+    @message = Message.new
+    @messages = @group.messages.includes(:user)
+  end
 
-	def create
-		@messages = @group.messages.new(messages_params)
-	end
+  def create
+    @messages = @group.messages.new(messages_params)
+  end
 
-	private
-	def messages_params
-		params.require(:messages).permit(:body, :image).merge(user_id: current_user.id)
-	end
+  private
+  def messages_params
+    params.require(:messages).permit(:body, :image).merge(user_id: current_user.id)
+  end
 
-	def set_group
-		@group = Group.find(params[:group_id])
-	end
+  def set_group
+    @group = Group.find(params[:group_id])
+  end
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,8 +1,8 @@
 class Message < ApplicationRecord
 
-	belongs_to :user
-	belongs_to :group
+  belongs_to :user
+  belongs_to :group
 
-	validates :body, presence: true, unless: :image?
-	mount_uploader :image, ImageUploader
+  validates :body, presence: true, unless: :image?
+  mount_uploader :image, ImageUploader
 end
